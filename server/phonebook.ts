7553381c757f4610ef5f3af5cdb702616ec00136
olsiodobashi <olsiodobashi@gmail.com>
2024-03-04 21:13:37 -0500

    enhancement: add missing search requirement

--- server/phonebook.ts
+++ server/phonebook.ts
@@ -22,6 +22,11 @@ export class PhoneBook {
       });
    }
 
+   private isDuplicatePhoneNumber(phoneNumber: string): boolean {
+      const customerIds = this.phoneCustomer.get(phoneNumber);
+      return customerIds ? customerIds.length > 1 : false;
+    }
+
    /**
     * Stores customer record
     * @param customer Customer data to be stored
@@ -35,7 +40,11 @@ export class PhoneBook {
     * @param phoneNumber Phone number data to be stored
     */
    public addPhoneNumber(phoneNumber: PhoneNumber) {
-      return this.phoneNumbers.set(phoneNumber.number, phoneNumber);
+      if (!this.isDuplicatePhoneNumber(phoneNumber.number)) {
+         this.phoneNumbers.set(phoneNumber.number, phoneNumber);
+      }
+
+      return this.phoneNumbers;
    }
 
    /**
@@ -53,5 +62,33 @@ export class PhoneBook {
          this.phoneCustomer.set(phoneNumber, []);
       }
       this.phoneCustomer.get(phoneNumber)!.push(customerId);
+   }
+
+   public searchByCustomerName(name: string): Customer[] {
+      const result: Customer[] = [];
+
+      for (let customer of this.customers.values()) {
+         if (
+            customer.firstName.toLowerCase().includes(name.toLowerCase()) ||
+            customer.lastName.toLowerCase().includes(name.toLowerCase())
+         ) {
+            result.push(customer);
+         }
+      }
+
+      return result;
+   }
+
+   public searchByPhoneNumber(phoneNumber: string): Customer[] {
+      const matchingCustomers: Customer[] = [];
+
+      for (let customer of this.customers.values()) {
+        const matches = customer.phoneNumbers.some(record => record.number === phoneNumber);
+        if (matches) {
+          matchingCustomers.push(customer);
+        }
+      }
+
+      return matchingCustomers;
    }
 }
